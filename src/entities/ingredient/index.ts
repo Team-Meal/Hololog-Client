85697f13b615bcbf9cb4d7922eedf29851e62e8d
koreahghg <koreahghg@gmail.com>
2026-06-24 08:24:13 +0900

    feat(ingredient): 식자재 Zustand 스토어 및 배럴 업데이트

--- src/entities/ingredient/index.ts
+++ src/entities/ingredient/index.ts
@@ -3,5 +3,18 @@ export type {
   IngredientStatus,
   IngredientCategory,
   IngredientUnit,
+  IngredientItem,
+  IngredientDetail,
+  CreateIngredientPayload,
+  PatchIngredientPayload,
+  PatchIngredientResponse,
 } from "./model/types";
 export { MOCK_INGREDIENTS } from "./model/mock";
+export { useIngredientStore } from "./model/ingredient.store";
+export {
+  getIngredientsApi,
+  createIngredientApi,
+  getIngredientApi,
+  patchIngredientApi,
+  deleteIngredientApi,
+} from "./api/ingredient.api";
