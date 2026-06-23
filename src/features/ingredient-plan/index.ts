0804306492ea536ab1db0f7177c86b58a60ef354
koreahghg <koreahghg@gmail.com>
2026-06-23 21:06:04 +0900

    feat(ingredient-plan): 피처 슬라이스 공개 API 업데이트

--- src/features/ingredient-plan/index.ts
+++ src/features/ingredient-plan/index.ts
@@ -1,2 +1,3 @@
 export { getIngredientPlanById, getIngredientPlans } from "./api/ingredient-plan.api";
 export { useIngredientPlanStore } from "./model/ingredient-plan.store";
+export { PlanFormModal } from "./ui/PlanFormModal";
